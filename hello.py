642458a7fcf93a32847cd6d43ad14732b206ad7a
fadam003 <45648319+fadam003@users.noreply.github.com>
2018-12-27 21:40:04 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+'''Exercise 2: Hello
+(9 Lines)
+Write a program that asks the user to enter his or her name. The program should
+respond with a message that says hello to the user, using his or her name.
+'''
+
+# ask user to enter her name ==> input
+user_name = input('Enter your name : ')
+# respnd with message says hello user name ==> output
+print('hello {}'.format(user_name))
