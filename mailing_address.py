dc66da1f2a5305d94356f63954090502b5748547
fadam003 <45648319+fadam003@users.noreply.github.com>
2018-12-27 21:33:52 -0800

    Update mailing_address.py

--- mailing_address.py
+++ mailing_address.py
@@ -1,1 +1,16 @@
 
+'''Exercise 1: Mailing Address
+(Solved—9 Lines)
+Create a program that displays your name and complete mailing address formatted in
+the manner that you would usually see it on the outside of an envelope. Your program
+does not need to read any input from the user.
+'''
+
+# display my-name ==> FADAM && my complete mailing address ==> 795 E DRAGRAM
+# TUCSON AZ 85705 USA
+print('''
+FADAM 
+795 E DRAGRAM
+TUCSON AZ 85705
+USA
+''')
